7d5791826cf7af2673bbb57368269030530cf94e
aifalabsglobal <kiran@aimtechnologies.in>
2025-11-25 14:06:06 +0530

    Refactor PrismaClient import and usage

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,20 +3,18 @@ config()
 
 import { Pool } from 'pg'
 import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient as PrismaClientAccelerator } from '@prisma/client'
+import { PrismaClient as PrismaAccelerator } from '@prisma/client'
 
-const connectionString = process.env.DATABASE_URL
+const connectionString = process.env.DATABASE_URL!
 
 const pool = new Pool({ connectionString })
 const adapter = new PrismaPg(pool)
 
-// This is the correct type now
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClientAccelerator | undefined
+  prisma: PrismaAccelerator | undefined
 }
 
-export const db = globalForPrisma.prisma ?? new PrismaClientAccelerator({ adapter })
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
+export const db =
+  globalForPrisma.prisma ?? new PrismaAccelerator({ adapter })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
